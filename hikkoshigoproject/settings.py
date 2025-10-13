3a55ebbf39e4ec859c96e83f3fac3ce6834686bb
ktsukkmddc <kailua0831@gmail.com>
2025-10-13 11:57:02 +0900

    パスワード再設定関連_3

--- hikkoshigoproject/settings.py
+++ hikkoshigoproject/settings.py
@@ -56,7 +56,10 @@ ROOT_URLCONF = 'hikkoshigoproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'app', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
