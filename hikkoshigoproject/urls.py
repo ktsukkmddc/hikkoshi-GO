371fe078c3c95768ef581f1734ab578bfbdc3d94
ktsukkmddc <kailua0831@gmail.com>
2025-11-03 01:18:15 +0900

    カレンダー調整とパスワード変更画面

--- hikkoshigoproject/urls.py
+++ hikkoshigoproject/urls.py
@@ -21,6 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
+    
     # パスワードリセット関連
     path('password_reset/', 
          auth_views.PasswordResetView.as_view(template_name='password_reset.html'),
@@ -34,4 +35,12 @@ urlpatterns = [
     path('reset/done/', 
          auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
          name='password_reset_complete'),
+    
+    # パスワード変更(ログインユーザー用)追加
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
+         name='password_change'),
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
+         name='password_change_done'),
 ]
